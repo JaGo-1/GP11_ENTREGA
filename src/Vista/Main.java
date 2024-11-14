f653aa6f4096c37f012189664786d6f9f9f707bb
SantiMGodoy <santiagomgodoy9@gmail.com>
2024-11-14 14:54:59 -0300

    Cambios en validaciones

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,4 +1,5 @@
 package Vista;
+
 import Modelo.MenuDiario;
 import Modelo.RenglonDeMenu;
 import java.sql.Connection;
@@ -11,57 +12,48 @@ import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
-        
-        
-            if (con != null) {
+
+        if (con != null) {
             System.out.println("¡Conexión exitosa a la base de datos!");
 
-            }
-            
+        }
+
 //            ComidaData cd = new ComidaData();
 //            Comida comida = new Comida("Yogur con frutos secos", "Desayuno","Yogur Griego, Almendras, Nueces, Avellanas, Pistachos", 150,  true );
 //            Comida comida2 = new Comida("Yogur con frutos secos 22222", "Desayuno 2222","Yogur Griego asdasdasd, Almendras, Nueces, Avellanas, Pistachos", 150,  true );
 //            
 //            cd.guardarComida(comida);
 //            cd.guardarComida(comida2);
-         //   List<Comida> comidas = cd.listarComidas();
-            
-         //   System.out.println(comidas);
-            
-                  MenuDiarioData md = new MenuDiarioData();
-           MenuDiario menu_diario = new MenuDiario();
-            
-           // md.guardarMenuDiario(menu_diario);
-     List<MenuDiario> menuDiarios = md.obtenerMenuDiariosPorDieta(18, 1);  // Dieta con codDieta = 1
-      List<MenuDiario> menuDiarios1 = md.listarMenusDiarios();
-            
-                System.out.println ("todo mal hacen" );
-              System.out.println (menuDiarios.size() );
-             
-           for (MenuDiario menuDiario : menuDiarios) {
-               
-                System.out.println ("entra" );
-               
-               
-               System.out.println("Menú Diario (CodMenu: " + menuDiario.getCodMenu() + ", Día: " + menuDiario.getDiaNro() + ")");
-               
-               for (RenglonDeMenu renglon : menuDiario.getRenglones()) {
-                  System.out.println("\tComida: " + renglon.getComida().getNombre() + ", Calorías: " + renglon.getSubTotalCalorias());
-                }
-               
-           }
-         
-         
-            FlatLightLaf.setup(); //LIBRERIA PARA LA GUI
-            Principal frame = new Principal();
-            frame.setVisible(true);
-       
-            
-}
+        //   List<Comida> comidas = cd.listarComidas();
+        //   System.out.println(comidas);
+//                  MenuDiarioData md = new MenuDiarioData();
+//           MenuDiario menu_diario = new MenuDiario();
+//            
+        // md.guardarMenuDiario(menu_diario);
+//     List<MenuDiario> menuDiarios = md.obtenerMenuDiariosPorDieta(18, 1);  // Dieta con codDieta = 1
+//      List<MenuDiario> menuDiarios1 = md.listarMenusDiarios();
+//            
+//                System.out.println ("todo mal hacen" );
+//              System.out.println (menuDiarios.size() );
+//             
+//           for (MenuDiario menuDiario : menuDiarios) {
+//               
+//                System.out.println ("entra" );
+//               eso
+//               
+//               System.out.println("Menú Diario (CodMenu: " + menuDiario.getCodMenu() + ", Día: " + menuDiario.getDiaNro() + ")");
+//               
+//               for (RenglonDeMenu renglon : menuDiario.getRenglones()) {
+//                  System.out.println("\tComida: " + renglon.getComida().getNombre() + ", Calorías: " + renglon.getSubTotalCalorias());
+//                }
+//               
+        FlatLightLaf.setup(); //LIBRERIA PARA LA GUI
+        Principal frame = new Principal();
+        frame.setVisible(true);
+
+    }
 }
